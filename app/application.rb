2269bec8aec9249aed176bcec2eac9807258f76c
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-07 21:05:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,27 +7,6 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
  
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
-    end
- 
-    resp.finish
-  end
- 
-end
-Notice that we only changed the puts statements into resp.write statements. That's it! Web servers are just big ruby apps that respond to the user in an HTTP response rather than via puts statements. Let's give the user a bit more information about what numbers they received by writing the numbers to the response as well. The #write method can be called many times to build up the full response. The response isn't sent back to the user until #finish is called.
-
-class Application
- 
-  def call(env)
-    resp = Rack::Response.new
- 
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
- 
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
     resp.write "#{num_3}\n"
@@ -41,4 +20,4 @@ class Application
     resp.finish
   end
  
-e
+end
